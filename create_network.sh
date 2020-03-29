5baed80ae5c9f215cec59a1c8797ae77013691dd
Andreas <andreas@jungierek.de>
2020-03-29 14:06:20 +0200

    divided all into more stacks

---
+++ create_network.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# junand 29.03.2020
+
+# TODO check Option --internal
+docker network create --driver bridge proxy
+docker network create --driver bridge backbone
+docker network create --driver bridge metrics
+###docker network create --driver bridge --internal logging

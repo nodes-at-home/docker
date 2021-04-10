4ffaeb2e56053476e96c8e92fb3a1bcde0064719
Andreas <andreas@jungierek.de>
2021-04-10 23:47:16 +0200

    collecting fritzbox data

--- database/create_buckets.sh
+++ database/create_buckets.sh
@@ -49,19 +49,23 @@ function list_buckets {
 
 BUCKET_ID_LOGS=$(get_bucket_id logs)
 BUCKET_ID_TELEGRAF=$(get_bucket_id telegraf)
+BUCKET_ID_FRITZBOX=$(get_bucket_id fritzbox)
 
 echo ""
 echo "bucket ids"
 echo "logs:             ${BUCKET_ID_LOGS}"
 echo "telegraf:         ${BUCKET_ID_TELEGRAF}"
+echo "fritzbox:         ${BUCKET_ID_FRITZBOX}"
 
 USER_ID_LOGS=$(get_user_id logs)
 USER_ID_TELEGRAF=$(get_user_id telegraf)
+USER_ID_FRITZBOX=$(get_user_id fritzbox)
 
 echo ""
 echo "user ids"
 echo "logs:             ${USER_ID_LOGS}"
 echo "telegraf:         ${USER_ID_TELEGRAF}"
+echo "fritzbox:         ${USER_ID_FRITZBOX}"
 
 
 TOKEN_LOGS_READ_WRITE=$(get_token logs_read_write logs "\
@@ -74,14 +78,21 @@ TOKEN_TELEGRAF_READ_WRITE=$(get_token telegraf_read_write telegraf "\
 --write-bucket ${BUCKET_ID_TELEGRAF} \
 ")
 
+TOKEN_FRITZBOX_READ_WRITE=$(get_token fritzbox_read_write fritzbox "\
+--read-bucket ${BUCKET_ID_FRITZBOX} \
+--write-bucket ${BUCKET_ID_FRITZBOX} \
+")
+
 echo ""
 echo "token"
 echo "logs_read_write:  ${TOKEN_LOGS_READ_WRITE}"
 echo "telegraf_read_write:  ${TOKEN_TELEGRAF_READ_WRITE}"
+echo "fritzbox_read_write:  ${TOKEN_FRITZBOX_READ_WRITE}"
 
 
 _=$(set_retention ${BUCKET_ID_LOGS} "4w")
 _=$(set_retention ${BUCKET_ID_TELEGRAF} "1w")
+_=$(set_retention ${BUCKET_ID_FRITZBOX} "4w")
 
 echo ""
 echo "set retention"

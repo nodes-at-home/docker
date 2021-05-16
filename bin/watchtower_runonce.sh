b9d1f3ff530b9ea164e3b7f2bbd9abe2f0df2df7
Andreas <andreas@jungierek.de>
2021-05-16 12:40:37 +0200

    added container name to watchtower runonce script

--- bin/watchtower_runonce.sh
+++ bin/watchtower_runonce.sh
@@ -2,4 +2,4 @@
 
 # junand 14.03.2021
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --run-once
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock --name watchtower_once containrrr/watchtower --run-once

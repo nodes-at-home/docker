c6e03305b146816a6ea72c89f047f4bfed075747
Andreas <andreas@jungierek.de>
2022-03-12 20:02:17 +0100

    watchtower_once command acepts container names

--- bin/watchtower_runonce.sh
+++ bin/watchtower_runonce.sh
@@ -2,4 +2,5 @@
 
 # junand 14.03.2021
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock --name watchtower_once containrrr/watchtower --run-once
+# possible options --dubug --trace
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ~/.docker/config.json:/config.json --name watchtower_once containrrr/watchtower --run-once $@

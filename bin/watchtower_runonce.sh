ea845d8d1361033c708922a1014a3cd3576a9685
Andreas <andreas@jungierek.de>
2022-03-29 13:20:15 +0200

    changed docker registry for watchtower runonce script

--- bin/watchtower_runonce.sh
+++ bin/watchtower_runonce.sh
@@ -3,4 +3,4 @@
 # junand 14.03.2021
 
 # possible options --dubug --trace
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ~/.docker/config.json:/config.json --name watchtower_once containrrr/watchtower --run-once $@
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ~/.docker/config.json:/config.json --name watchtower_once ${DOCKER_REGISTRY}containrrr/watchtower --run-once $@

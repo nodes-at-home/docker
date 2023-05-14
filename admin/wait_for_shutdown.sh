4eda5f7c8a664867f83e610b7f2e4b1592b960a0
Andreas Jungierek <andreas@jungierek.de>
2023-05-14 08:45:54 +0200

    added doc for shutdown service

--- admin/wait_for_shutdown.sh
+++ admin/wait_for_shutdown.sh
@@ -2,7 +2,15 @@
 
 # junand 06.05.2023
 # https://stackoverflow.com/questions/42660690/is-it-possible-to-shut-down-the-host-machine-by-executing-a-command-on-one-of-it
-# install inotify-tools before
+#
+# install instructions
+# (1) install inotify-tools before
+# (2) sudo cp wait_for_shutdown /lib/systemd/system and change owner/rights
+# (3) sudo systemctl daemon-reload
+# (4) sudo systemctl enable wait_for_shutdown.service
+# (5) sudo systemctl start wait_for_shutdown.service
+# the signal file is linked into home assistant container for rule based signaling of a shutdown
+
 
 SIGNAL_FILE=/var/tmp/shutdown_signal
 

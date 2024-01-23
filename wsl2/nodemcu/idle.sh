a0b253a01a89a61a17e060a3044aa926affa3344
Andreas <andreas@jungierek.de>
2024-01-23 22:20:05 +0100

    container for building nodemcu firmware

---
+++ wsl2/nodemcu/idle.sh
@@ -0,0 +1,20 @@
+#!/bin/bash                                                                
+                                                   
+# https://gist.github.com/ikbear/56b28f5ecaed76ebb0ca
+                                                
+echo "This is a idle script (infinite loop) to keep container running."    
+echo "Please replace this script."                                         
+                                                                           
+cleanup ()                                                                 
+{                                                                          
+  kill -s SIGTERM $!                                                         
+  exit 0                                                                     
+}                                                                          
+                                                                           
+trap cleanup SIGINT SIGTERM                                                
+                                                                           
+while [ 1 ]                                                                
+do                                                                         
+  sleep 60 &                                                             
+  wait $!                                                                
+done

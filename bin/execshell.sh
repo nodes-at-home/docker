7fb8135c87a4d5425dd3cc7e795a188c86c875c7
Andreas Jungierek <andreas@jungierek.de>
2025-11-13 06:21:23 +0000

    container exec with optional platform suffix

--- bin/execshell.sh
+++ bin/execshell.sh
@@ -9,13 +9,47 @@ fi
 
 [ -z "$1" ] && echo "ERROR: first parameter must bee the app to execute!" && exit
 
+find_stack () {
+
+    echo $( grep "$1:$" ./*/docker-compose.yaml | cut -d/ -f2 )
+
+}
+
+platform () {
+
+    result="$(uname -n)"
+
+    case "${result}" in
+        andreas-book4*)    result=wsl2;;
+    esac
+
+    echo ${result}
+
+}
+
 CONTAINER_SHELL=bash
 [ ! -z "$2" ] && CONTAINER_SHELL=$2
 
 pushd "$HOME/docker/docker"
 
 APP=$1
-STACK=`grep "${APP}:$" ./*/docker-compose.yaml | cut -d/ -f2`
+
+STACK=$(find_stack "$APP")
+# echo "step 1 STACK=${STACK}"
+
+if [ -z "$STACK" ]
+then
+    APP="${APP}-$(platform)"
+    STACK=$(find_stack "$APP")
+    # echo "step 2 STACK=${STACK}"
+fi
+
+if [ -z "$STACK" ]
+then
+    echo "ERROR: could not find stack for app '$APP'"
+    popd
+    exit 1
+fi
 
 echo "Executing $APP in $STACK"
 

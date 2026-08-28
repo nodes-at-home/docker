780a4b6e31588e799a4926612af1ffd414162e22
Andreas Jungierek <andreas@jungierek.de>
2026-08-28 20:56:16 +0200

    cleaup some scripts

--- bin/registry_gc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker exec -it -u root registry registry garbage-collect /var/lib/registry/registry_config.yaml --delete-untagged=true
--- bin/watchtower_runonce.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# junand 14.03.2021
-
-# possible options --dubug --trace
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ~/.docker/config.json:/config.json --name watchtower_once ${DOCKER_REGISTRY}nickfedor/watchtower --run-once $@
